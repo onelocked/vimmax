ecde07458088b16905cd8b0bdd660461e6bea2dc
onelocked <onelock@mail.com>
2026-03-02 19:51:05 +0000

    [blink-cmp] add ripgrep-cmp

--- modules/plugins/blink-cmp.nix
+++ modules/plugins/blink-cmp.nix
@@ -1,9 +1,36 @@
 {
   flake.modules.plugins.blink-cmp = {
     plugins = {
+      blink-ripgrep.enable = true;
       blink-cmp = {
         enable = true;
         settings = {
+          sources = {
+            sources.default = [
+              "ripgrep"
+            ];
+            providers = {
+              ripgrep = {
+                async = true;
+                module = "blink-ripgrep";
+                name = "Ripgrep";
+                score_offset = 100;
+                opts = {
+                  prefix_min_len = 3;
+                  context_size = 5;
+                  max_filesize = "1M";
+                  project_root_marker = ".git";
+                  project_root_fallback = true;
+                  search_casing = "--ignore-case";
+                  additional_rg_options = { };
+                  fallback_to_regex_highlighting = true;
+                  ignore_paths = { };
+                  additional_paths = { };
+                  debug = false;
+                };
+              };
+            };
+          };
           completion.menu.border = "rounded";
           snippets.preset = "luasnip";
           completion = {
