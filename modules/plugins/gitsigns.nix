f7a5089e283fae8763700d0123e67dc4075cca63
onelocked <onelock@mail.com>
2026-03-03 19:21:39 +0000

    [gin-signs] add keybinds to show git blame

--- modules/plugins/gitsigns.nix
+++ modules/plugins/gitsigns.nix
@@ -1,6 +1,11 @@
 { self, ... }:
 {
   flake.modules.plugins.gitsigns =
+    { lib, ... }:
+    let
+      inherit (self.mkKey) mkKeymap;
+      inherit (lib.nixvim) mkRaw;
+    in
     {
       plugins.gitsigns = {
         enable = true;
@@ -15,5 +20,13 @@
           };
         };
       };
+      keymaps = [
+        # Blame
+        (mkKeymap "n" "<leader>gk" (mkRaw "function() require('gitsigns').blame_line({ full = true }) end")
+          "Blame Line (Full)"
+        )
+        (mkKeymap "n" "<leader>gK" ":lua require('gitsigns').blame()<CR>" "Blame File")
+      ];
+
     };
 }
