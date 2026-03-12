d6a70a3331486ac7a09acaeb7d3a784c4a694870
onelocked <onelock@mail.com>
2026-03-12 02:57:17 +0000

    [yazi] open for directories

--- modules/plugins/yazi.nix
+++ modules/plugins/yazi.nix
@@ -3,7 +3,16 @@
   flake.modules.plugins.yazi = {
 
     plugins = {
-      yazi.enable = true;
+      yazi = {
+        enable = true;
+        settings = {
+          enable_mouse_support = false;
+          floating_window_scaling_factor = 0.8;
+          open_for_directories = true;
+          yazi_floating_window_border = "single";
+        };
+
+      };
     };
     keymaps =
       let
