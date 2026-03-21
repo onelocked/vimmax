7cc6af045966b52de1c723c61fac9d0413a22ede
onelocked <onelock@mail.com>
2026-03-21 14:18:59 +0000

    [lazygit] correctly find project even in different buffers

--- modules/plugins/lazygit.nix
+++ modules/plugins/lazygit.nix
@@ -1,17 +1,26 @@
+{ self, ... }:
 {
-  flake.modules.plugins.lazygit = {
-    plugins.lazygit = {
-      enable = true;
+  flake.modules.plugins.lazygit =
+    { lib, ... }:
+    let
+      inherit (lib.nixvim) mkRaw;
+      inherit (self.mkKey) mkKeymap;
+    in
+    {
+      plugins.lazygit = {
+        enable = true;
+      };
+
+      keymaps = [
+        (mkKeymap "n" "<leader>gg" (mkRaw ''
+          function()
+            local buf_dir = vim.fn.expand("%:p:h")
+            local git_root = vim.fn.systemlist("git -C " .. vim.fn.shellescape(buf_dir) .. " rev-parse --show-toplevel")[1]
+            local cwd = (git_root and vim.v.shell_error == 0) and git_root or buf_dir
+            vim.cmd("lcd " .. vim.fn.fnameescape(cwd))
+            vim.cmd("LazyGit")
+          end
+        '') "LazyGit")
+      ];
     };
-
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>gg";
-        action = "<cmd>LazyGit<CR>";
-        options.desc = "LazyGit";
-      }
-    ];
-
-  };
 }
