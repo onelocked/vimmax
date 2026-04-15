4ced6a88879d1890e7b5a0869465d5b2bcf7ba27
onelocked <onelock@mail.com>
2026-04-15 02:17:30 +0100

    [flake] add systems input

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
 
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    systems.url = "github:nix-systems/x86_64-linux";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs = {
         flake-parts.follows = "flake-parts";
         nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
       };
     };
     flake-parts = {
