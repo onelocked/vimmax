e63486f8b1b860d3a130c4f5ced1f01d2f78d968
onelocked <onelock@mail.com>
2026-04-18 20:42:11 +0100

    [plugins] yazi fix plugin use full lua and extrapackages

--- modules/plugins/yazi.nix
+++ modules/plugins/yazi.nix
@@ -1,26 +1,18 @@
-{ self, ... }:
 {
-  flake.modules.plugins.yazi = {
-
-    plugins = {
-      yazi = {
-        enable = true;
-        settings = {
-          enable_mouse_support = false;
-          floating_window_scaling_factor = 0.8;
-          open_for_directories = true;
-          yazi_floating_window_border = "single";
-        };
-
-      };
+  flake.modules.plugins.yazi =
+    { pkgs, ... }:
+    {
+      extraPlugins = [ pkgs.vimPlugins.yazi-nvim ];
+      extraConfigLua = ''
+        require('yazi').setup {
+          yazi_command = "yazi",
+          open_for_directories = true,
+          floating_window_scaling_factor = 0.8,
+          yazi_floating_window_border = "single"
+        }
+        vim.keymap.set('n', '<leader>y', function()
+          require('yazi').yazi()
+        end, { desc = 'File Explorer (Yazi)' })
+      '';
     };
-    keymaps =
-      let
-        inherit (self.mkKey) mkKeymap;
-      in
-      [
-        (mkKeymap "n" "<leader>y" "<cmd>Yazi<cr>" "Open Yazi")
-      ];
-
-  };
 }
