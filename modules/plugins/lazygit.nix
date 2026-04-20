0162b0c6786c93710420d11ae385d9e943c3a303
onelocked <onelock@mail.com>
2026-04-20 01:26:03 +0100

    [plugins] lazygit stop using the module

--- modules/plugins/lazygit.nix
+++ modules/plugins/lazygit.nix
@@ -1,37 +1,18 @@
-{ self, ... }:
 {
   flake.modules.plugins.lazygit =
-    { lib, ... }:
-    let
-      inherit (lib.nixvim) mkRaw;
-      inherit (self.mkKey) mkKeymap;
-    in
+    { pkgs, ... }:
     {
-      plugins.lazygit = {
-        enable = true;
-        settings = {
-          floating_window_border_chars = [
-            "╭"
-            "─"
-            "╮"
-            "│"
-            "╯"
-            "─"
-            "╰"
-            "│"
-          ];
-        };
-      };
-      keymaps = [
-        (mkKeymap "n" "<leader>gg" (mkRaw ''
-          function()
-            local buf_dir = vim.fn.expand("%:p:h")
-            local git_root = vim.fn.systemlist("git -C " .. vim.fn.shellescape(buf_dir) .. " rev-parse --show-toplevel")[1]
-            local cwd = (git_root and vim.v.shell_error == 0) and git_root or buf_dir
-            vim.cmd("lcd " .. vim.fn.fnameescape(cwd))
-            vim.cmd("LazyGit")
-          end
-        '') "LazyGit")
-      ];
+      extraPlugins = [ pkgs.vimPlugins.lazygit-nvim ];
+      extraConfigLua = ''
+        vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }
+
+        vim.keymap.set('n', '<leader>gg', function()
+          local buf_dir = vim.fn.expand("%:p:h")
+          local git_root = vim.fn.systemlist("git -C " .. vim.fn.shellescape(buf_dir) .. " rev-parse --show-toplevel")[1]
+          local cwd = (git_root and vim.v.shell_error == 0) and git_root or buf_dir
+          vim.cmd("lcd " .. vim.fn.fnameescape(cwd))
+          vim.cmd("LazyGit")
+        end, { desc = "LazyGit" })
+      '';
     };
 }
