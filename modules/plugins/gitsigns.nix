ef82b6076928f932ec2a47151b1df97495d3b04a
onelocked <onelock@mail.com>
2026-04-20 14:50:23 +0100

    plugins: gitsigns use raw lua, and improve config

--- modules/plugins/gitsigns.nix
+++ modules/plugins/gitsigns.nix
@@ -1,32 +1,46 @@
 { self, ... }:
 {
   flake.modules.plugins.gitsigns =
-    { lib, ... }:
+    { lib, pkgs, ... }:
     let
       inherit (self.mkKey) mkKeymap;
       inherit (lib.nixvim) mkRaw;
+      inherit (self.icons.ui) LineLeft Triangle BoldLineLeft;
     in
     {
-      plugins.gitsigns = {
-        enable = true;
-        settings = {
-          current_line_blame = true;
-          signs = with self.icons.ui; {
-            add.text = "${LineLeft}";
-            change.text = "${LineLeft}";
-            delete.text = "${LineLeft}";
-            topdelete.text = "${Triangle}";
-            changedelete.text = "${BoldLineLeft}";
-          };
-        };
-      };
+      extraPlugins = [ pkgs.vimPlugins.gitsigns-nvim ];
+
+      extraConfigLua = # lua
+        ''
+          require('gitsigns').setup({
+            current_line_blame = true,
+            word_diff = false,
+            signs = {
+              add = { text = '${LineLeft}' },
+              change = { text = '${LineLeft}' },
+              delete = { text = '${LineLeft}' },
+              topdelete = { text = '${Triangle}' },
+              changedelete = { text = '${BoldLineLeft}' },
+            },
+            preview_config = {
+              border = 'rounded',
+              style = 'minimal',
+              relative = 'cursor',
+              row = 0,
+              col = 1
+            },
+          })
+        '';
+
       keymaps = [
-        # Blame
         (mkKeymap "n" "<leader>gk" (mkRaw "function() require('gitsigns').blame_line({ full = true }) end")
           "Blame Line (Full)"
         )
         (mkKeymap "n" "<leader>gK" ":lua require('gitsigns').blame()<CR>" "Blame File")
+        (mkKeymap "n" "<leader>gf"
+          (mkRaw "function() require('gitsigns').toggle_linehl(); require('gitsigns').toggle_deleted() end")
+          "Toggle Inline Diff"
+        )
       ];
-
     };
 }
