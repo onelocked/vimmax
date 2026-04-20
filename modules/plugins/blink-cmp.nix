37c8940547e6d1f49658204a3b464cf5b35174c1
onelocked <onelock@mail.com>
2026-04-20 14:17:57 +0100

    blink-cmp rewrite, break out into lua

--- modules/plugins/blink-cmp.nix
+++ modules/plugins/blink-cmp.nix
@@ -1,93 +1,51 @@
 {
-  flake.modules.plugins.blink-cmp = {
-    plugins = {
-      blink-ripgrep.enable = true;
-      blink-cmp-words.enable = true;
-      blink-cmp = {
-        enable = true;
-        settings = {
-          sources = {
-            default = [
-              "buffer"
-              "lsp"
-              "path"
-              "ripgrep"
-              "dictionary"
-              "snippets"
-              "codeium"
-            ];
-            providers = {
-              codeium = {
-                name = "codeium";
-                module = "codeium.blink";
-                score_offset = 1000;
-              };
-              path = {
-                score_offset = 300;
-              };
-              snippets = {
-                score_offset = 250;
-              };
-              lsp = {
-                score_offset = 200;
-              };
-              dictionary = {
-                score_offset = 50;
-                name = "blink-cmp-words";
-                module = "blink-cmp-words.dictionary";
-                opts = {
-                  dictionary_search_threshold = 3;
-                  score_offset = 0;
-                  definition_pointers = [
-                    "!"
-                    "&"
-                    "^"
-                  ];
-                };
-              };
-              ripgrep = {
-                async = true;
-                module = "blink-ripgrep";
-                name = "Ripgrep";
-                score_offset = 100;
-                opts = {
-                  prefix_min_len = 3;
-                  context_size = 5;
-                  max_filesize = "1M";
-                  project_root_marker = ".git";
-                  project_root_fallback = true;
-                  search_casing = "--ignore-case";
-                  additional_rg_options = { };
-                  fallback_to_regex_highlighting = true;
-                  ignore_paths = { };
-                  additional_paths = { };
-                  debug = false;
-                };
-              };
-            };
-          };
-          # per_filetype = {
-          #   text = [ "dictionary" ];
-          #   markdown = [ "thesaurus" ];
-          # };
-          completion.menu.border = "rounded";
-          snippets.preset = "luasnip";
-          completion = {
-            list.selection.preselect = true;
-            documentation = {
-              auto_show = false;
-              auto_show_delay_ms = 0;
-            };
-          };
-          keymap = {
-            "<Tab>" = [
-              "accept"
-              "fallback"
-            ];
-          };
-        };
-      };
-    };
+  flake.modules.plugins.blink-cmp =
+    { pkgs, ... }:
+    {
+      extraPlugins = with pkgs.vimPlugins; [
+        blink-cmp
+        blink-ripgrep-nvim
+      ];
 
-  };
+      extraConfigLua = ''
+        require('blink.cmp').setup({
+          sources = {
+            default = { "lsp", "path", "snippets", "buffer", "ripgrep", "codeium" },
+            providers = {
+              ripgrep = {
+                module = "blink-ripgrep",
+                name = "Ripgrep",
+                score_offset = 100,
+                opts = {
+                  prefix_min_len = 3,
+                  context_size = 5,
+                  max_filesize = "1M",
+                  project_root_marker = ".git",
+                  project_root_fallback = true,
+                  search_casing = "--ignore-case",
+                },
+              },
+              codeium = {
+                name = "codeium",
+                module = "codeium.blink",
+                score_offset = 1000,
+              },
+            },
+          },
+          completion = {
+            list = { selection = { preselect = true } },
+            menu = { border = "rounded" },
+            documentation = {
+              auto_show = false,
+              auto_show_delay_ms = 0,
+            },
+          },
+          snippets = { preset = "luasnip" },
+          keymap = {
+            preset = 'default',
+            ['<Tab>'] = { 'accept', 'fallback' },
+          },
+        })
+      '';
+    };
 }
