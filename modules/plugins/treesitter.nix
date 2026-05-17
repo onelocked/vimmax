d33073cf73143d75593756f1e54af742661891d8
onelocked <onelock@mail.com>
2026-05-17 22:35:40 +0100

    treesitter fix

--- modules/plugins/treesitter.nix
+++ modules/plugins/treesitter.nix
@@ -5,17 +5,17 @@
       plugins = {
         treesitter = {
           enable = true;
+          highlight = {
+            enable = true;
+            disable = [
+              "markdown"
+            ];
+          };
           settings = {
             indent.enable = true;
             folding.enable = true;
             autopairs.enable = true;
             incremental_selection.enable = true;
-            highlight = {
-              enable = true;
-              disable = [
-                "markdown"
-              ];
-            };
             grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
             auto_install = false;
             ensureInstalled = [
