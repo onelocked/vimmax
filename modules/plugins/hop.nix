185f63d28a09a7cf39ac6def1859b97a4640f748
onelocked <onelock@mail.com>
2026-05-29 15:20:37 +0100

    hop work pure black colors for hopping

--- modules/plugins/hop.nix
+++ modules/plugins/hop.nix
@@ -1,19 +1,35 @@
 { self, ... }:
 {
-  m.plugins.hop = {
-    plugins.hop = {
-      enable = true;
-      settings = {
-        virtual_cursor = true;
-        quit_key = "<Esc>";
-        teasing = true;
+  m.plugins.hop =
+    { theme, lib, ... }:
+    {
+      extraConfigLua = ''
+        ${lib.optionalString (theme == "dark") # lua
+          ''
+            vim.api.nvim_set_hl(0, "HopUnmatched", { fg = "#000000" })
+            vim.api.nvim_create_autocmd("ColorScheme", {
+              pattern = "*",
+              callback = function()
+                vim.api.nvim_set_hl(0, "HopUnmatched", { fg = "#000000" })
+              end,
+            })
+          ''
+        }
+      '';
+
+      plugins.hop = {
+        enable = true;
+        settings = {
+          virtual_cursor = true;
+          quit_key = "<Esc>";
+          teasing = true;
+        };
       };
+      keymaps =
+        with self.mkKey;
+        map (mode: mkKeymap mode "<leader>hw" "<cmd>HopWord<cr>" "Hop to Word") [
+          "n"
+          "v"
+        ];
     };
-    keymaps =
-      with self.mkKey;
-      map (mode: mkKeymap mode "<leader>hw" "<cmd>HopWord<cr>" "Hop to Word") [
-        "n"
-        "v"
-      ];
-  };
 }
