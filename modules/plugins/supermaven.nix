8eddf651598df48111d7c01db0c984306612668b
onelocked <180497738+onelocked@users.noreply.github.com>
2026-08-11 23:00:32 +0100

    supermaven: disabled until toggled on

--- modules/plugins/supermaven.nix
+++ modules/plugins/supermaven.nix
@@ -4,22 +4,31 @@
     {
       extraPlugins = [ pkgs.vimPlugins.supermaven-nvim ];
       extraConfigLua = ''
-        require("supermaven-nvim").setup({
-          keymaps = {
-            accept_suggestion = "<F19>",
-            clear_suggestion = "<C-c>",
-            accept_word = "<C-Right>",
-          },
-
-          ignore_filetypes = {  },
-          color = {
-            suggestion_color = "#85819c",
-            cterm = 244,
-          },
-          log_level = "off", -- set to "off" to disable logging completely
-          disable_inline_completion = false, -- disables inline completion for use with cmp
-          disable_keymaps = false, -- disables built in keymaps for more manual control
-        })
+        vim.keymap.set("n", "<Leader>sm", function()
+          -- If it hasn't been loaded yet, set it up and start it
+          if not _G.SupermavenLoaded then
+            require("supermaven-nvim").setup({
+              keymaps = {
+                accept_suggestion = "<F19>",
+                clear_suggestion = "<C-c>",
+                accept_word = "<C-Right>",
+              },
+              ignore_filetypes = { },
+              color = {
+                suggestion_color = "#85819c",
+                cterm = 244,
+              },
+              log_level = "off",
+              disable_inline_completion = false,
+              disable_keymaps = false,
+            })
+            _G.SupermavenLoaded = true
+            vim.notify("Supermaven awakened! (sm hehe)", vim.log.levels.INFO)
+          else
+            -- If it's already set up, just toggle it on/off
+            vim.cmd("SupermavenToggle")
+          end
+        end, { noremap = true, silent = true, desc = "Toggle Supermaven" })
       '';
     };
 }
